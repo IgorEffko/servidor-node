f565efa6b6179b72756415913d9b97b8d11710ec
magalhaesf <fernanda.fatima.magalhaes@escola.pr.gov.br>
2026-09-01 09:34:06 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// Importar o modulo nativo 'http' do Node.js
+const http = require('http');
+
+// Define o endereço (localhost - Minha maquina local) e a porta onde o servidor vai escutar
+const hostname = '127.0.0.1';
+const port =  3000;
+
+// Cria o servidor web
+const server = http.createServer( (req, res) => {
+// Define o status http como 200 (OK) e o tipo de conteudo como texto plano em UTF
+} );
