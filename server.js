28b35e57498b9d689a946feae78ac07189e41657
IgorEffko <igor.vendruscolo@escola.pr.gov.br>
2026-09-08 08:36:38 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
+// Importa o módulo nativo 'http' do Node.js
+const http = require('http');
 
-//define o endereço do servidor (localhost) e a porta (3000) que o servidor irá escutar
-
+// Define o endereço (localhost) e a porta onde o servidor vai escutar
 const hostname = '127.0.0.1';
 const port = 3000;
 
-//cria o servidor e define a função de callback que será executada a cada requisição recebida
+// Cria o servidor web
+const server = http.createServer((req, res) => {
+ if (req.url === '/' ) {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    return res.end( '<h1>Página Inicial<h1>' ); // O return impede a execução das linhas de baixo
 
-    const server = http.createServer((req, res) => {
-    res.statusCode = 200; //define o status da resposta como 200 (OK) 
-    res.setHeader('Content-Type', 'text/plain'); //define o tipo de conteúdo da resposta como texto simples
-    res.end('Hello World\n'); //envia a resposta "Hello World" e finaliza a resposta
-    });
+ }
 
-// //faz o servidor escutar na porta e endereço definidos anteriormente
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`); //exibe uma mensagem no console informando que o servidor está rodando
-// });
+ if (req.url === '/alunos' ) {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    return res.end( '<h1>Lista de Alunos<h1>' ); 
+ }
 
-// // Para executar o servidor, abra o terminal, navegue até o diretório onde o arquivo server.js está localizado e execute o comando: node server.js
+    // Define o status HTTP como 200 (OK) e o tipo de conteúdo como texto plano em UTF-8
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+ 
+  // Envia a resposta para o navegador/cliente
+  res.end('<h1>Olá, alunos! O servidor Node.js está rodando com sucesso!\n<h1>');
 
 
-// const server = http.createServer(() => {
-// });
+});
 
-const nome = (a, b) => {
-//   return a + b;
-}
+// Faz o servidor começar a escutar na porta definida
+server.listen(port, hostname, () => {
+  console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
